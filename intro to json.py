cc7c596b44bf69743de43c1021836f8474fdbccc
Bezza Zelalem <146134832+ZZ5454@users.noreply.github.com>
2023-11-08 10:28:07 -0800

    Add files via upload

---
+++ intro to json.py
@@ -0,0 +1,14 @@
+#
+import json
+
+
+#Create the data dictionary
+
+data= {
+
+    'name': 'John Doe',
+    'age':25,
+    'city':'Seattle,WA'
+    'interests': ['Sports','Music','Movies'],
+    'Student': True
+}
