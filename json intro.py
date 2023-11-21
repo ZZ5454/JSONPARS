a5dd71ec2ff649e67473528a93291449f1d3ae7f
ZZ5454 <bezzazelalem@gmail.com>
2023-11-20 23:18:23 -0800

    Finished writing comments for python file

--- json intro.py
+++ json intro.py
@@ -1,4 +1,4 @@
-#
+# Import the JSON module for working with JSON data
 import json
 
 
@@ -13,15 +13,16 @@ data = {
     'Student': True
     }
 
-##
+## Write the initial data to a JSON file
 with open('data.json','w') as json_file:
 
     json.dump(data,json_file,indent=4)
 
+# Print a message indicating that data has been written to data.json
 print('Data has been written to data.json')
 
 
-#
+# Read data from the json file
 with open('data.json',"r") as json_file:
 
     loaded_data = json.load(json_file)
@@ -29,7 +30,7 @@ with open('data.json',"r") as json_file:
 print ("Successfully able to read data.json")
 print (loaded_data)
 
-#
+# Modify the loaded data in the json file
 loaded_data['age'] = 100
 loaded_data['interests'].append('travel')
 
@@ -37,5 +38,6 @@ loaded_data['interests'].append('travel')
 with open('data.json', 'w') as json_file:
     json.dump(loaded_data, json_file, indent=4)
 
+# Print a message indicating that modified data has been written to data.json
 print('Modified data written to data.json')
     
